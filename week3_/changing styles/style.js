e2e0102563105b915fbff88dff1ed8b6365c03e8
Seanerlars <Sean.larsen@bryanuniversity.edu>
2020-10-11 18:52:49 -0500

    changing styles

---
+++ week3_/changing styles/style.js
@@ -0,0 +1,15 @@
+for (var i = 0; i < 5; i++){
+    var newH2 = document.createElement("h2")
+    newH2.textContent = "Hello World"
+    document.body.append(newH2)
+    newH2.style.fontSize = "20px"
+    newH2.style.fontWeight="lighter"
+    newH2.style.fontFamily="sans-serif"
+    newH2.style.color="cornflowerblue"
+    }
+    document.classList.add("border");
+
+
+
+  
+    
