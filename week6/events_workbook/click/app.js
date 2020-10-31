63066e1dabb6f3070cf9f3fd6e1956f33b1d64fa
Seanerlars <Sean.larsen@bryanuniversity.edu>
2020-10-30 19:55:14 -0500

    events workbook

---
+++ week6/events_workbook/click/app.js
@@ -0,0 +1,8 @@
+// make the box disapear when the user clicks it
+
+var box = document.getElementById("redbox")
+
+box.addEventListener("click", function (){
+    console.log("its been clicked")
+box.style.display = "none"})
+
